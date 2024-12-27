e6c617127e63f97996f851e5e2f8fe8b14c07cf8
maz-s3d3 <mazozisaad@gmail.com>
2024-12-27 22:03:26 +0100

    commit 2

--- Desktop/html Projects/React/argan_beauty/.history/src/tailwind.config_20241222181115.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  darkMode: 'class', // Permet de basculer entre les thèmes clair et sombre
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
